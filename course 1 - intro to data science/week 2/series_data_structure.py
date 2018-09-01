a728a108d387c28aa1c8992361cdb72ce0d5a1d7
Timothy <te25son@gmail.com>
2018-09-01 19:08:01 +0200

    add week 2 / 'series data structure' lesson examples

---
+++ course 1 - intro to data science/week 2/series_data_structure.py
@@ -0,0 +1,39 @@
+"""
+Course 1
+Week 2
+The Series Data Structure
+"""
+
+import pandas as pd
+import numpy as np
+
+animals = ['Tiger', 'Lion', 'Bear']
+print(pd.Series(animals))
+
+numbers = [1, 2, 3]
+print(pd.Series(numbers))
+
+animals = ['Lion', 'Tiger', None]
+print(pd.Series(animals))
+
+numbers = [1, 2, None]
+print(pd.Series(numbers))  # None is returned as NaN (not a number) by pandas
+
+print(np.nan == None)  # False
+
+print(np.nan == np.nan)  # False
+
+print(np.isnan(np.nan))  # True
+
+sports = {
+    'Archery': 'Bhutan',
+    'Golf': 'Scotland',
+    'Sumo': 'Japan',
+    'Taekwondo': 'South Korea'
+}
+s = pd.Series(sports)
+print(s)
+print(s.index)
+
+s = pd.Series(['Tiger', 'Lion', 'Bear'], index=['India', 'Angola', 'America'])
+print(s)
